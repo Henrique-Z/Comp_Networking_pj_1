717c5230c2cfb5789e0acf3007a0e31cfeb2bf11
Joseph <57154021+Henrique-Z@users.noreply.github.com>
2024-08-02 19:13:29 -0300

    Update server_app.py

--- app_server/server_app.py
+++ app_server/server_app.py
@@ -1,16 +1,10 @@
 import socketserver
 import http.server
-#from start_server import start_IPv4_server
-
 
 HOST = "0.0.0.0"
 PORT = 4400
 HTTPD = None
 
-
-# starts server:
-#new_server = start_IPv4_server(PORT)
-#new_server.close()
 
 try:
 	print("[>>> servidor:] iniciando...")
@@ -20,7 +14,7 @@ try:
 	print("[>>> servidor:] servindo na porta:", PORT)
 
 	HTTPD.serve_forever() # ignores the timeout attribute
-except Exception as exc:
+except Exception as e:
 	if HTTPD is not None:
 		HTTPD.shutdown()     # encerra todos os pedidos
 		HTTPD.server_close() # fecha o socket que estava "escutando" a porta
