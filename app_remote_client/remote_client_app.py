2d61f85e13656f214bf2f2b520c5cb0dc7712c93
Joseph <57154021+Henrique-Z@users.noreply.github.com>
2024-08-02 19:12:20 -0300

    Update remote_client_app.py

--- app_remote_client/remote_client_app.py
+++ app_remote_client/remote_client_app.py
@@ -1,16 +1,25 @@
+import webbrowser
+import os
 from start_remote_client import start_remote_client
 
 
-HOST = "192.168.39.1081" # IP local do host servidor
-PORT = 4400		 # número da porta TCP
+HOST = "192.168.39.101" # IP local do host servidor
+PORT = 4400				# número da porta TCP
 
 
-try:
-	conn, response_data = start_remote_client(HOST, PORT)
-except TypeError as e:
-	print("[>>> cliente:] host do servidor não informado")
-	raise
+# estabelece conexão do servidor:
+conn, response_data = start_remote_client(HOST, PORT)
+
+INDEX_PATH = "index.html" # "local_buffer/index.html"
+with open(INDEX_PATH, "w", encoding="utf-8") as index:
+	index.write(response_data)
+
+# abre arquivo "index.html" no browser padrão do sistema:
+webbrowser.open(f'file://{os.path.abspath(INDEX_PATH)}')
 
 # encerra a conexão com o servidor:
 print("[>>> cliente:] encerrando a conexão com o servidor...")
 conn.close()
+
+# remove templetes html:
+#os.remove(INDEX_PATH)
