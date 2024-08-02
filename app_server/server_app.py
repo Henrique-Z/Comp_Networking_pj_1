8c5af43fe075097873994153f203ca65e848961d
Joseph <57154021+Henrique-Z@users.noreply.github.com>
2024-08-02 15:59:54 -0300

    Update server_app.py

--- app_server/server_app.py
+++ app_server/server_app.py
@@ -1,10 +1,27 @@
-from start_server import start_IPv4_server
+import socketserver
+import http.server
+#from start_server import start_IPv4_server
 
 
-#HOST = "::" # allow server to listen to all network interfaces
+HOST = "0.0.0.0"
 PORT = 4400
+HTTPD = None
 
 
 # starts server:
-new_server = start_IPv4_server(PORT)
+#new_server = start_IPv4_server(PORT)
 #new_server.close()
+
+try:
+	print("[>>> servidor:] iniciando...")
+
+	HANDLER = http.server.SimpleHTTPRequestHandler
+	HTTPD = socketserver.TCPServer((HOST, PORT), HANDLER)
+	print("[>>> servidor:] servindo na porta:", PORT)
+
+	HTTPD.serve_forever() # ignores the timeout attribute
+except Exception as exc:
+	if HTTPD is not None:
+		HTTPD.shutdown()     # encerra todos os pedidos
+		HTTPD.server_close() # fecha o socket que estava "escutando" a porta
+	raise
