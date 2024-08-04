1ff1c763de0d39b010e52affa98edbef2a9b5fc8
Joseph <jh.delucas@gmail.com>
2024-08-04 18:25:41 -0300

    implementa servidor local para comunicação com browser(imccompleto)

--- app_remote_client/remote_client_app.py
+++ app_remote_client/remote_client_app.py
@@ -1,24 +1,68 @@
-import webbrowser
 import os
+import webbrowser
+
 from start_remote_client import start_remote_client
+from start_local_server import start_local_server, timestamp
 
 
-HOST = "192.168.39.101" # IP local do host servidor
-PORT = 4400				# número da porta TCP
+HOST = "192.168.206.101" # wlp1s0 IPv4 do servidor primário
+PORT = 4400				 # para conexão com servidor primário
+LH_PORT = 8000			 # para conexão com browser
+
+TEMP_DIR_PATH = "server_files"
+INDEX_PATH = TEMP_DIR_PATH + "/index.html"
+STYLE_PATH = TEMP_DIR_PATH + "/style.css"
 
 
 # estabelece conexão do servidor:
 conn, response_data = start_remote_client(HOST, PORT)
 
-INDEX_PATH = "index.html" # "local_buffer/index.html"
-with open(INDEX_PATH, "w", encoding="utf-8") as index:
-	index.write(response_data)
+# cria servidor web local:
+local_server, local_server_ip = start_local_server("localhost", LH_PORT)
+
+if not os.path.exists(TEMP_DIR_PATH):
+	# cria diretório temporário para o armazenamento dos
+	# arquivos vindos do servidor, caso ainda não exista:
+	os.mkdir(TEMP_DIR_PATH)
+	print(f"{local_server_ip} - - {timestamp()} >>> localserver: diretório '{TEMP_DIR_PATH}' criado no diretório atual.")
+
+# cria "index.html" na pasta temporária:
+index_html = os.open(INDEX_PATH, flags = os.O_WRONLY | os.O_CREAT) # os.O_CREAT: cria arquivo, caso não exista
+
+# armazena bit-stream 'response_data' em "index.html":
+os.write(index_html, response_data)
+
+# realiza a requisição de "style.css":
+conn.request("GET", "/html_templates/style.css")
+http_response = conn.getresponse()
+print(f"{local_server_ip} - - {timestamp()} >>> cliente: status da resposta:", http_response.status)
+print(f"{local_server_ip} - - {timestamp()} >>> cliente: motivo da resposta:", http_response.reason)
+
+response_data = http_response.read()
+
+# cria "style.css" na pasta temporária:
+style_css = os.open(STYLE_PATH, flags = os.O_WRONLY | os.O_CREAT) # os.O_CREAT: cria arquivo, caso não exista
+
+# armazena bit-stream 'response_data' em "style.css":
+os.write(style_css, response_data)
 
 # abre arquivo "index.html" no browser padrão do sistema:
-webbrowser.open(f'file://{os.path.abspath(INDEX_PATH)}')
+BASE_URL = "http://" + local_server_ip + ":" + str(LH_PORT) + "/"
+FULL_URL = BASE_URL + INDEX_PATH
+try:
+	webbrowser.open(FULL_URL, 2) # opens 'FULL_URL' in new tab
+except webbrowser.Error as e:
+	print(f"{local_server_ip} - - {timestamp()} >>> localserver: conexão com o browser falhou.")
+	print(">>> Error: {e}")
+	raise
 
-# encerra a conexão com o servidor:
-print("[>>> cliente:] encerrando a conexão com o servidor...")
+# encerra a conexão com o browser:
+#local_server.shutdown()	 # interrompe o loop de processamento de solicitações
+							 # encerra a thread
+#local_server.server_close() # fecha o socket
+
+# encerra a conexão com o servidor primário:
+print(f"{local_server_ip} - - {timestamp()} >>> localserver: encerrando a conexão com o servidor...")
 conn.close()
 
 # remove templetes html:
--- app_remote_client/start_remote_client.py
+++ app_remote_client/start_remote_client.py
@@ -1,5 +1,12 @@
 import http.client
+import time
 
+def timestamp():
+	# retorna data e hora local em string formatada:
+	return time.strftime(
+        "[%d-%m-%Y  %H:%M:%S]",
+        time.localtime()
+        )
 
 def start_remote_client(host = None, port = 4400):
 
@@ -8,22 +15,20 @@ def start_remote_client(host = None, port = 4400):
 
 	conn = http.client.HTTPConnection(host, port)
 
-	# estabelece os parâmetros necessários para estabelecer o `request`:
+	# estabelece os parâmetros necessários para estabelecer o 'request':
 	conn.request("GET", "/html_templates/index.html")
 
-	# encaminha o `request` e receber a `response`:
-	http_response = conn.getresponse() # variável do tipo `HTTPResponse`
-	print("[>>> cliente:] status da resposta:", http_response.status)
-	print("[>>> cliente:] reason da resposta:", http_response.reason)
+	# encaminha o 'request' e receber a 'response':
+	http_response = conn.getresponse() # variável da classe 'HTTPResponse'
+	print(f"_________ - - {timestamp()} >>> cliente: status da resposta: {http_response.status}")
+	print(f"_________ - - {timestamp()} >>> cliente: motivo da resposta: {http_response.reason}")
 
-	response_data = http_response.read().decode()
-	print("[>>> cliente:] dados na resposta:", response_data)
+	response_data = http_response.read()#.decode()
+	#print(">>> cliente: dados na resposta:", response_data)
 
 	response_headers = http_response.getheaders()
 	if response_headers:
-		print("[>>> cliente:] headers da resposta:\n\t",
-			  response_headers[0][0], ":",
-			  response_headers[0][1]
-			  )
+		print(f"_________ - - {timestamp()} >>> cliente: headers da resposta:")
+		print("                                     >>>", response_headers[0][0], ":", response_headers[0][1])
 
 	return conn, response_data
